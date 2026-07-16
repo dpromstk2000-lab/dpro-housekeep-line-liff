3597205af921c2154f062bb30cbcf47809a8720a
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-16 21:21:55 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DPRO_HOUSEKEEP_CONFIG = Object.freeze({
+  API_BASE: 'https://dpro-housekeep-line-api.dpromstk2000.workers.dev',
+  COMPANY_CODE: 'dpro_housekeep_demo',
+  LIFF_ID: '',
+  VERSION: 'HOUSEKEEP-3-CUSTOMER-RESERVATION-20260716'
+});
